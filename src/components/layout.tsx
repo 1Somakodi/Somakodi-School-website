2814c829d8a4c70cab823de47ff236355b4c470c
1somakodi <info.somakodi@gmail.com>
2026-01-01 08:55:23 +0300

    updated layout
    added google analytics

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,31 @@
 "use client";
 
 import Navbar from "@/components/navbar";
+// app/layout.tsx
+import Script from "next/script";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX');
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
